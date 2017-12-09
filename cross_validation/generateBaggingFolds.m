aab7452cff884c75415fffe6b7539a83b6551b9f
Erik Reinertsen <erikrtn@gmail.com>
2017-12-09 12:24:19 -0500

    features and labels are now structs, to be consistent with formatting of crossfold generator code

--- cross_validation/generateBaggingFolds.m
+++ cross_validation/generateBaggingFolds.m
@@ -1,9 +1,10 @@
-function [idx, features_cv, labels_cv] = generateBaggingFolds(features, labels, normalizeFlag, p)
+function [idx, features_cv, labels_cv] = generateBaggingFolds(features, labels, normalizeFlag, p, numInstances)
 
-% [idx, features_cv, labels_cv] = generateBaggingFolds(features, labels, validFold, p)
 
-% Initialize default parameter values
-
+% Default: bag five times
+if nargin < 5
+    numInstances = 5;
+end
 
 % Default: 70:30 training:test ratio
 if nargin < 4
@@ -15,33 +16,42 @@ if nargin < 3
     normalizeFlag = 1;
 end
 
-% Initialize other values
-unique_labels = unique(labels)';
-idx.train = [];
-idx.test = [];
+for i = 1:numInstances
 
-for k = unique_labels
-    ind_kth_O = find(labels==k);
-    r_ind = ind_kth_O(randperm(length(ind_kth_O)));
-    idx.train = [idx.train       ; r_ind(1:floor(p * length(r_ind)))];
-    idx.test = [idx.test         ; r_ind(floor(p * length(r_ind))+1:end) ];
-end
+    features_cv(i).train = [];
+    features_cv(i).test = [];
+    labels_cv(i).train = [];
+    labels_cv(i).test = [];
+    
+    % Initialize other values
+    unique_labels = unique(labels)';
+    idx.train = [];
+    idx.test = [];
 
-idx.train = idx.train(randperm(length(idx.train)));
-idx.test = idx.test(randperm(length(idx.test)));
+    for k = unique_labels
+        ind_kth_O = find(labels==k);
+        r_ind = ind_kth_O(randperm(length(ind_kth_O)));
+        idx.train = [idx.train; r_ind(1:floor(p * length(r_ind)))];
+        idx.test = [idx.test  ; r_ind(floor(p * length(r_ind))+1:end)];
+    end
+    
+    idx.train = idx.train(randperm(length(idx.train)));
+    idx.test = idx.test(randperm(length(idx.test)));
 
-if nargout > 1
-    % Develop the training set
-    features_cv.train = features(idx.train, :);
-    labels_cv.train = labels(idx.train);
+    if nargout > 1
+        % Develop training set
+        features_cv(i).train = features(idx.train, :);
+        labels_cv(i).train = labels(idx.train);
 
-    % Develop the testing set
-    features_cv.test = features(idx.test, :);
-    labels_cv.test = labels(idx.test);
-end
+        % Develop testing set
+        features_cv(i).test = features(idx.test, :);
+        labels_cv(i).test = labels(idx.test);
+    end
 
-% Normalize data against training set
-if normalizeFlag
-    [features_cv.train, norm_info] = normalizeFeatures(features_cv.train);
-    [features_cv.test] = normalizeFeatures(features_cv.test, norm_info);
+    % Normalize data against training set
+    if normalizeFlag
+        [features_cv(i).train, norm_info] = normalizeData(features_cv(i).train);
+        [features_cv(i).test] = normalizeData(features_cv(i).test, norm_info);
+    end
+    
 end
